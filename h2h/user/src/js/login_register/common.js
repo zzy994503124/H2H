0cd1cc0e9ba7dbba102eccaaa3b97aaad83b15ad
zzy994503124 <zzy1994530>
2016-01-03 20:54:51 +0800

    删除了没用的css，js，文件夹

---
+++ h2h/user/src/js/login_register/common.js
@@ -0,0 +1,96 @@
+//打开字滑入效果
+window.onload = function(){
+	$(".connect p").eq(0).animate({"left":"0%"}, 600);
+	$(".connect p").eq(1).animate({"left":"0%"}, 400);
+};
+//jquery.validate表单验证
+$(document).ready(function(){
+	//登陆表单验证
+	$("#loginForm").validate({
+		rules:{
+			password:{
+				required:true,
+				minlength:3,
+				maxlength:32,
+			},
+			email:{
+				required:true,//必
+				isEmail:true,
+			},
+			identifyingCode:{
+				required:true,//必填
+			}
+		},
+		//错误信息提示
+		messages:{
+			password:{
+				required:"必须填写密码",
+				minlength:"密码至少为20个字符",
+				maxlength:"密码至多为20个字符",
+			},
+			email:{
+
+				required:"请输入邮箱地址",
+				isEmail: "必须是校内邮箱"
+			},
+			identifyingCode:{
+				required:"请填写验证码",//必填
+			}
+		},
+
+	});
+	//注册表单验证
+	$("#registerForm").validate({
+		rules:{
+			password:{
+				required:true,
+				minlength:6,
+				maxlength:20,
+			},
+			email:{
+				required:true,
+				isEmail:true,
+			},
+			confirm_password:{
+				required:true,
+				minlength:3,
+				equalTo:'.password'
+			},
+			identifyingCode:{
+				required:true,//必填
+			},
+			username:{
+				required:true,//必填
+			}
+		},
+		//错误信息提示
+		messages:{
+			password:{
+				required:"必须填写密码",
+				minlength:"密码至少为6个字符",
+				maxlength:"密码至多为20个字符",
+			},
+			email:{
+				required:"请输入邮箱地址",
+				isEmail: "必须是校内邮箱"
+			},
+			confirm_password:{
+				required: "请再次输入密码",
+				minlength: "确认密码不能少于3个字符",
+				equalTo: "两次输入密码不一致",//与另一个元素相同
+			},
+			identifyingCode:{
+				required:"请填写验证码",//必填
+			},
+			username:{
+				required:"请填写用户名",//必填
+			}
+		},
+	});
+	//添加自定义验证规则
+	jQuery.validator.addMethod("isEmail", function(value, element) {
+		var length = value.length;
+		var email_number = /^\d{8}@bjtu.edu.cn$/;
+		return this.optional(element) || (length === 20 && email_number.test(value));
+	}, "必须是校内邮箱");
+});
---
+++ h2h/user/src/js/order/order.js
@@ -0,0 +1,72 @@
+/**
+ * Created by msi on 2016/5/3.
+ */
+function gotoHome(){
+    window.location.href="/";
+}
+$('#number').change(function(){
+    var rentDayStr = $('#number').val();
+    var priceStr = $('#price').html();
+    var rentDayInt = parseInt(rentDayStr);
+    var priceFloat = parseFloat(priceStr);
+    $('#sumPrice').html(parseFloat(rentDayInt * priceFloat).toFixed(2));
+    $('#totalPrices').html(parseFloat(rentDayInt * priceFloat).toFixed(2));
+});
+function isNull(str){
+    if ( str == "" ) return true;
+    var regu = "^[ ]+$";
+    var re = new RegExp(regu);
+    return re.test(str);
+}
+$('#submit').click(function(){
+    var rentDay = $('#number').val();
+    var startDate = $('#startDate').val();
+    var endDate = $('#endDate').val();
+    var name = $('#name').val();
+    var address = $('#address').val();
+    var phoneNum = $('#phoneNumber').val();
+
+    var success = true;
+    if(parseInt(rentDay)<=0){
+        $('#number').css("border","2px solid crimson");
+        success = false;
+    }else{
+        $('#number').css("border","1px solid darkturquoise");
+    }
+    //j检查是否为空
+    if(isNull(name)){
+        $('.nameContainer').addClass("has-error");
+        $('#name').css("border","2px solid crimson");
+        success = false;
+    }
+    else{
+        $('.nameContainer').addClass("has-success").removeClass("has-error");
+        $('#name').css("border","2px solid darkturquoise");
+    }
+    if(isNull(address)){
+        $('.addressContainer').addClass("has-error");
+        $('#address').css("border","2px solid crimson");
+        success = false;
+    }else{
+        $('.addressContainer').addClass("has-success").removeClass("has-error");
+        $('#address').css("border","1px solid darkturquoise");
+    }
+    if(isNull(phoneNum)){
+        $('.phoneNumContainer').addClass("has-error");
+        $('#phoneNumber').css("border","2px solid crimson");
+        success =false;
+    }else if(!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(phoneNum))){
+        $('.phoneNumContainer').addClass("has-error");
+        $('#cotactInfo .hint').html("* 手机号码格式不对！");
+        $('#cotactInfo .hint').css("visibility","visible");
+        success = false;
+    }else{
+        $('.phoneNumContainer').addClass("has-success").removeClass("has-error");
+        $('#cotactInfo .hint').html("");
+        $('#cotactInfo .hint').css("visibility","hideen");
+        $('#phoneNumber').css("border","1px solid darkturquoise");
+    }
+    if(success){
+        $('#submit').attr("data-target","#myModal");
+    }
+});
