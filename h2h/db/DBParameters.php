e6ba50fa7622244018a5935c612d576e3a4fee09
Jason-CPY <chpy_work@163.com>
2016-04-13 16:35:49 +0800

    完善弹窗
    路径以及命名的变化

---
+++ h2h/db/DBParameters.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zlg
+ * Date: 2016/4/8
+ * Time: 13:11
+ */
+class DBParameters
+{
+    public static $dbname = "h2h";
+    public static $server = "localhost";
+    public static $username = "root";
+    public static $psd = "";
+
+}
