af75de044b71b982a6f4784ca3c4c65cf9140f4c
zhangligao <zhangligao9509@gmail.com>
2016-04-13 16:18:40 +0800

    修改主页

--- h2h/db/DBBooks.php
+++ h2h/db/DBBooks.php
@@ -6,16 +6,17 @@
  * Date: 2016/4/10
  * Time: 11:48
  */
-require_once DBParameters;
+//require_once dirname(__FILE__).'\DBParameters';
 class DBBooks
 {
 
-    private $conn;
-
+    private $conn = null;
     public function connect()
     {
-        $this->conn = new mysqli(DBParameters::$server, DBParameters::$username,
-            DBParameters::$psd, DBParameters::$dbname);
+     /*   $this->conn = new mysqli(DBParameters::$server, DBParameters::$username,
+            DBParameters::$psd, DBParameters::$dbname);*/
+        $this->conn = new mysqli("localhost","root",
+        		"", "h2h");
         if($this->conn->connect_error)
         {
             die("Connection error".$this->conn->connect_error);
@@ -28,9 +29,14 @@ class DBBooks
      */
     public function getBookIds()
     {
-        $sql = "SELECT bookId from book";
+    	$this->connect();
+        $sql = "SELECT * from book";
         $result = $this->conn->query($sql);
-        return $result;
+        while ($row = mysqli_fetch_array($result))
+        	$rows[] = $row;
+        return $rows;
+      //  return $result.toString();
+        $this->disconnect();
         
     }
 
@@ -39,10 +45,26 @@ class DBBooks
      * 返回 数组 每个元素包含 书名 作者 图片文件夹路径
      * @param $ids id列表
      */
-    public function getSimpleInfo($id){
+    public function getSimpleInfo(){
+    	$this->connect();
+    	$sql = "SELECT bookNumber, rentPrice from book_rent";
+    	$result = $this->conn->query($sql);
+    	while ($row = mysqli_fetch_array($result))
+        	$rows[] = $row;
+        	return $rows;
+    	$this->disconnect();
+    }
+    /**
+     * 获取简略信息
+     * 返回 数组 每个元素包含 书名 作者 图片文件夹路径
+     * @param $ids id列表
+     */
+    public function getSimpleInfoById($id){
+    	$this->connect();
         $sql = "SELECT bookNumber, rentPrice from book_rent where bookId = $id";
         $result = $this->conn->query($sql);
         return $result;
+        $this->disconnect();
     }
 
     /**
