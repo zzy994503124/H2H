5a4b10de7f60992af29659b83ec821513393c827
zhangligao <zhangligao9509@gmail.com>
2016-04-19 17:39:57 +0800

    unit test code

--- h2h/db/test/DBBookstest.php
+++ h2h/db/test/DBBookstest.php
@@ -11,17 +11,42 @@ require_once "/../DBBooks.php";
 class DBBookstest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * id存在,查询书籍详情
+     */
     public function testGetBookDetails(){
+        $dbbooks = new DBBooks();
+        $dbbooks->getDetails(1);
+    }
+
+    /**
+     * id不存在,查询详情
+     */
+    public function testGetBookDetails10(){
         $dbbooks = new DBBooks();
         $dbbooks->getDetails(10);
     }
 
+    /**
+     * 获取ids
+     */
     public function testGetBookIds(){
         $dbbooks = new DBBooks();
         $dbbooks->getBookIds();
     }
 
+    /**
+     * 获取存在的书籍id
+     */
     public function testGetBookSimpleInfo(){
+        $dbbooks = new DBBooks();
+        $dbbooks->getSimpleInfoById(1);
+    }
+
+    /**
+     * 获取不存在的书籍的id
+     */
+    public function testGetBookSimpleInfo10(){
         $dbbooks = new DBBooks();
         $dbbooks->getSimpleInfoById(10);
     }
