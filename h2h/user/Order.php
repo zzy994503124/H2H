3985be03e565985482cd632bcfead80761930177
zzy994503124 <zzy1994530>
2016-04-19 18:20:04 +0800

    Order页对提交信息的验证
    防止姓名，手机号码，地址为空；
    验证手机号码格式。

--- h2h/user/Order.php
+++ h2h/user/Order.php
@@ -33,7 +33,7 @@
 					<tbody>
 						<tr>
 							<td>Data Structure</td>
-							<td><span id = "price"></span></td>
+							<td><span id = "price">2.00</span></td>
 							<td><input id = "number" type="text" value="1"/></td>
 							<td><span id = "sumPrice"></span></td>
 						</tr>
@@ -47,24 +47,35 @@
                 <br/>
                 <!--    租借时间          -->
                 <div class="datetimepicker"  data-date="16-04-2016" data-date-format="dd-mm-yyyy">
-                    <label>租借时间</label>
+                    <label for="startDate">租借时间</label>
                     <input id="startDate" size="16" type="text" class="form-control">
-                    
+                    <br />
+                    <p class="hint">aaaaa</p>
                 </div>
                 
                 <!--    归还时间          -->
                 <div class="datetimepicker"  data-date="16-04-2016" data-date-format="dd-mm-yyyy">
-                    <label>归还时间</label>
-                    <input id="endDate" size="16" type="text" class="form-control">
+                    <label for="endDate">归还时间</label>
+                    <input id="endDate" size="16" type="text" class="form-control" readonly="true">
+                    <br />
+                    <p class="hint">aaaaa</p>
                 </div>
 			</div>
 			<form id="cotactInfo">
 				<fieldset class="text-center">
 					<h2>联系信息</h2> 
-					<input class="form-control" placeholder="姓名" type="text" id="name"/>
-					<input class="form-control" placeholder="手机号码" type="text" id="phoneNumber"/><br />
-					<input class="form-control" placeholder="地址" type="text" id="address"/><br />
-					<button type="button" class="orderBtn" data-toggle="modal" data-target="#myModal" id="submit" onclick="">提  交</button>					
+                    <div class="nameContainer">
+                        <input class="form-control" placeholder="姓名" type="text" id="name"/>
+                    </div>
+					<div class="phoneNumContainer">
+                        <input class="form-control" placeholder="手机号码" type="text" id="phoneNumber"/><br />
+                    </div>
+					<div class="addressContainer">
+                        <input class="form-control" placeholder="地址" type="text" id="address"/><br />
+                    </div>
+					
+                    <p class="hint">aaaaa</p>
+					<button type="button" class="orderBtn" data-toggle="modal" data-target="" id="submit" onclick="">提  交</button>					
 					<button  class="orderBtn" id="cancel">取 消</button>
 				</fieldset>
 			</form>		
@@ -89,30 +100,85 @@
 </body>
  	<script src="js/jquery-1.11.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-     <script src="js/jquery.js"></script>
+    <script src="js/jquery.js"></script>
     <script src="js/jquery.datetimepicker.js"></script>
    
-     <script type="text/javascript">
-		window.onload = function(){
-			var url = location.search;
-			var parameters =  url.split("=")[1];
-			var id = parameters.split("&price")[0];
-			var price = url.split("=")[2];
-		
-			var number = document.getElementById("number").value;
-			//alert(number);
-			var sumPrice = price*number;
-			document.getElementById("price").innerHTML = sumPrice;
-			document.getElementById("sumPrice").innerHTML = sumPrice;
-			document.getEle
-            mentById("totalPrices").innerHTML = sumPrice;
-            //初始化日期选择器
+    <script type="text/javascript">
+        window.onload = function(){
+            var url = location.search;
+            var parameters =  url.split("=")[1];
+            var id = parameters.split("&price")[0];
+            var price = url.split("=")[2];
             
-		}
-		function gotoHome(){
-			window.location.href="/";
+            //alert(number);
+            var sumPrice = price*number;
+            document.getElementById("price").innerHTML = sumPrice;
+            document.getElementById("sumPrice").innerHTML = sumPrice;
+            mentById("totalPrices").innerHTML = sumPrice;
         }
+        function gotoHome(){
+            window.location.href="/";
+       }
         $('#startDate').datetimepicker();
-        $('#endDate').datetimepicker();
+        $('#number').change(function(){
+            var rentDayStr = $('#number').val();
+            var priceStr = $('#price').html();
+            var rentDayInt = parseInt(rentDayStr);
+            var priceFloat = parseFloat(priceStr);
+            $('#sumPrice').html(parseFloat(rentDayInt * priceFloat).toFixed(2));
+            $('#totalPrices').html(parseFloat(rentDayInt * priceFloat).toFixed(2));
+        });
+        function isNull(str){
+            if ( str == "" ) return true;
+            var regu = "^[ ]+$";
+            var re = new RegExp(regu);
+            return re.test(str);
+        }
+        $('#submit').click(function(){
+            var rentDay = $('#number').val();
+            var startDate = $('#startDate').val();
+            var endDate = $('#endDate').val();
+            var name = $('#name').val();
+            var address = $('#address').val();
+            var phoneNum = $('#phoneNumber').val();
+            
+            var success = true;
+            //j检查是否为空
+            if(isNull(name)){
+                $('.nameContainer').addClass("has-error");
+                $('#name').css("border","2px solid crimson");                
+                success = false;
+            }
+            else{
+                $('.nameContainer').addClass("has-success").removeClass("has-error");
+                $('#name').css("border","2px solid darkturquoise");
+            }
+            if(isNull(address)){
+                $('.addressContainer').addClass("has-error");
+                $('#address').css("border","2px solid crimson");
+                success = false;
+            }else{
+                $('.addressContainer').addClass("has-success").removeClass("has-error");
+                $('#address').css("border","2px solid darkturquoise");
+            }
+            if(isNull(phoneNum)){
+                $('.phoneNumContainer').addClass("has-error"); 
+                $('#phoneNumber').css("border","2px solid crimson");
+                success =false;
+            }else if(!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(phoneNum))){
+                $('.phoneNumContainer').addClass("has-error"); 
+                $('#cotactInfo .hint').html("* 手机号码格式不对！");                
+                $('#cotactInfo .hint').css("visibility","visible");
+                success = false;                
+            }else{
+                $('.phoneNumContainer').addClass("has-success").removeClass("has-error");
+                $('#cotactInfo .hint').html("");                
+                $('#cotactInfo .hint').css("visibility","hideen");
+                $('#phoneNumber').css("border","2px solid darkturquoise");
+            }           
+            if(success){
+                $('#submit').attr("data-target","#myModal");
+            }
+        });
 	</script>
 </html>
